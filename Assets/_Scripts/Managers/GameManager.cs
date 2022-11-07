4c71ff26362acb72aad8bbf3c0a6c66696bc6201
Ahmet Nazmi BOZHÜYÜK <67506232+ahmetnazmibozhuyuk@users.noreply.github.com>
2022-11-07 17:49:30 +0300

    dotween added

---
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace RitimUS.Managers
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+
+
+
+    }
+
+}
